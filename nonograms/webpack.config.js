2626cb06d6abd4aee56420516704fd9eff23fada
pirnataly <piroznatal@gmail.com>
2024-01-28 18:05:45 +0300

    init: start task

---
+++ nonograms/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // ...
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  entry: {
+    main: './src/script.js',
+  },
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin({ title: 'Hello World' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|mp3)$/i,
+        type: 'assets/resource',
+      },
+    ],
+  },
+};
