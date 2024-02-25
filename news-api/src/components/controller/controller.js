fb9ad7e60e2303b097f8706107bbf3a6ca9ba931
pirnataly <piroznatal@gmail.com>
2024-02-25 18:07:48 +0300

    feat: change config files

--- news-api/src/components/controller/controller.js
+++ news-api/src/components/controller/controller.js
@@ -17,10 +17,10 @@ class AppController extends AppLoader {
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
                 const sourceId = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                                if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
-                        {
+                                    {
                             endpoint: 'everything',
                             options: {
                                 sources: sourceId,
---
+++ news-api/src/index.ts
@@ -0,0 +1,6 @@
+import App from './components/app/app.js';
+import './global.css';
+
+const app = new App();
+app.start();
+console.log('hello');
