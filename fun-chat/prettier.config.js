a15e697c3efb828e42d3532409bd77a34a6a830b
pirnataly <piroznatal@gmail.com>
2024-04-15 22:54:49 +0300

    refactor: add linter, prettier, husky

--- fun-chat/prettier.config.js
+++ fun-chat/prettier.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    printWidth: 100,
-    singleQuote: true,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    tabWidth: 2,
-    semi: true,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
 };
+
---
+++ fun-chat/webpack.config.js
@@ -0,0 +1,54 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.ts'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpg|png|svg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.ts$/i,
+        use: 'ts-loader',
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new EslintPlugin({ extensions: ['ts'] }),
+  ],
+  resolve: {
+    alias: {
+      img: path.join(__dirname, 'src', 'components', 'view', 'img'),
+    },
+    extensions: ['.ts', '.js'],
+  },
+
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+};
