88d03cddba3520607a56d2d3afc7d94b3ac233b0
pirnataly <piroznatal@gmail.com>
2024-04-21 14:41:13 +0300

    feat: add info about interlocutor's name and status

--- fun-chat/src/app/components/main/main-section/main-section.ts
+++ fun-chat/src/app/components/main/main-section/main-section.ts
@@ -15,10 +15,21 @@ export default class MainSection {
 
   userlist: HTMLElement | HTMLDivElement | HTMLLinkElement;
 
+  chatHeader: HTMLElement | HTMLDivElement | HTMLLinkElement;
+
+  userToChatWith: HTMLElement | HTMLDivElement | HTMLLinkElement;
+
+  userToChatWithStatus: HTMLElement | HTMLDivElement | HTMLLinkElement;
+
   constructor() {
     this.mainSection = createElement('mainSection', 'section');
     this.usersListContainer = createElement('list-container', 'nav');
     this.chatContainer = createElement('chat-container', 'article');
+    this.chatHeader = createElement('chat-header', 'article');
+    this.userToChatWith = createElement('user-to-chat-width', 'label');
+    this.userToChatWithStatus = createElement('user-status', 'label');
+    this.chatHeader.append(this.userToChatWith, this.userToChatWithStatus);
+    this.chatContainer.append(this.chatHeader);
     this.searchinput = createInput(searchAttributes);
     this.searchinput.className = 'search-input';
     this.userlist = createElement('user-list', 'ul');
@@ -65,7 +76,10 @@ export default class MainSection {
       const { target } = ev;
       const li = (target as HTMLElement).closest('li');
       if (li) {
-        this.chatContainer.textContent = li.children[1].textContent;
+        this.userToChatWith.textContent = li.children[1].textContent;
+        this.userToChatWithStatus.textContent = li.children[0].classList.contains('user-active')
+          ? 'online'
+          : 'offline';
       }
     });
   }
